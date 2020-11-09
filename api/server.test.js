b3434a70a2f91fec288d1c5e083c481b9c5c9707
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-11-08 23:30:30 -0500

    two tests passing

---
+++ api/server.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest')
+
+const db = require('../database/dbConfig');
+
+const server = require('./server');
+
+describe('end point tests', function() {
+    describe('POST /register and POST /login', function() {
+        beforeAll(async() => {
+                await db('users').truncate();
+            })
+            
+        it('should return status 500', function() {
+                return request(server)
+                    .post('/api/auth/register')
+                    .send({ username: "JohnDoe12", password: "password123" })
+                    .then(res => {
+                        expect(res.status).toBe(500);
+                    })
+            })
+        it('response type should match json', function() {
+                return request(server)
+                    .post('/api/auth/register')
+                    .send({ username: "JohnDoe321", password: "password321" })
+                    .then(res => {
+                        expect(res.type).toMatch(/json/i);
+                    })
+            })
+    })
+}) 
